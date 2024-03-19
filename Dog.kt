16784f92fa052c9580582725f5e46e25e7d920b7
Abhishek-165 <abhishekdixit1651996@gmail.com>
2024-03-19 15:06:44 +0530

    "Data classed added"

--- Dog.kt
+++ Dog.kt
@@ -13,3 +13,14 @@ class  Dog constructor(val name: String){
 fun main(){
     var dog : Dog = Dog("Daisy")
 }
+
+
+
+// Data class are used to store data it also provide has code matching and other functionality
+
+data class Person(
+    val name : String,
+    val age : Int,
+    val designation : String
+
+)
