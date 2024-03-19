276ca196b5f21443761dc7c70267a99b1d1ab10a
Ritesh Dixit <ritesh@Riteshs-MacBook-Pro.local>
2024-03-19 10:21:14 +0530

    kotlin basics

---
+++ basicsOfKotlin.kt
@@ -0,0 +1,58 @@
+
+// DataTypes in Kotlin
+
+
+// Mutuable 
+
+var kbool : Boolean = true
+var kint : Int = 10
+var klong : Long = 10L
+var kfloat : Float = 10.0f
+var kdouble : Double = 10.0
+var kstring : String = "Abhishek"
+var kChar : Char = 'a' // only for single characters and not for strings, Use  only single quotes
+
+// Immutable
+val kInt : Int = 10
+val kLong : Long = 10
+val kFloat : Float = 10.0f
+val kDouble : Double = 10.0
+val kString : String = "Abhishek"
+val kBoolean : Boolean = true
+val kChar : Char = 'a'
+
+
+
+// special Characters in Kotlin
+
+// \t - Tab
+// \n - New Line
+// \\ - Backslash
+// \' - Single Quote
+// \" - Double Quote   
+
+
+// User inputs in kotlin
+
+var a : Int = readln().toInt()  // converts string to int
+
+
+// Condition Statement
+
+
+// if(age >=18 && age <= 60){        are same as       if(age in 18..60){
+// }else{
+//     println("eligible")
+// }
+/
+
+
+
+// Looping string in Kotlin
+fun main(){
+    for(i in "Abhishek"){
+        print(i)
+    }
+}
+
+
